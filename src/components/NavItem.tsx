b6593864962fc8bff01abaaf7a3a482164dd30e3
Mohammad Shaad Shaikh <mohammadshaadshaikh@gmail.com>
2023-04-11 06:53:57 +0530

    Added Navbar

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-const NavItem = ({ text, href, active }) => {
+const NavItem = ({ text, href, active }: {text: any; href: any; active: any}) => {
   return (
     <Link href={href} className={`nav__item ${
         active ? "text-gray-200" : "text-gray-500"
