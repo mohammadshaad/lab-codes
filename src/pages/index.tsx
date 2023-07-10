1af24e59e38f46e39a1993b9acfcf6db7e5e4352
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-07-10 15:51:33 +0530

    :fire: Added the CN content :zap:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import OS from '@/pages/Operating'
-import SE from '@/pages/Software'
-import Maths from '@/pages/Maths'
+import CN from '@/pages/Networks'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <OS />
-      <SE />
-      <Maths />
+      <CN />
     </main>
   )
 }
